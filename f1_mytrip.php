c02a35fb19a4f8feb8f57be1deeb466c50256af3
fencingworld <fencingworld@qq.com>
2014-11-12 12:44:38 +0800

    Create f1_mytrip.php

---
+++ f1_mytrip.php
@@ -0,0 +1,243 @@
+
+<!Doctype html>
+<html class="nx-main760">
+<head>
+<meta name="Description" content="" />
+<meta name="Keywords" content="" />
+<title>HappyTrip</title><meta charset="utf-8"/>
+<link rel="shortcut icon" type="image/x-icon" href="http://a.xnimg.cn/favicon-rr.ico?ver=3" />
+<link rel="apple-touch-icon" href="http://a.xnimg.cn/wap/apple_icon_.png" />
+<link rel="stylesheet" type="text/css" href="http://s.xnimg.cn/a72156/nx/core/base.css">
+   <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+   <script src="/scripts/jquery.min.js"></script>
+   <script src="/bootstrap/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+if(typeof nx === 'undefined'){
+var nx = {};
+}
+nx.log = {
+startTime : + new Date()
+};
+nx.user = {
+id : "518341325",
+ruid:"518341325",
+tinyPic	: "http://hdn.xnimg.cn/photos/hdn321/20141014/1725/tiny_e7r6_54a80001bfca1986.jpg ",
+name : "李可利",
+requestToken : '-1802925312',
+_rtk : '604a895a'
+};nx.user.isvip = false;nx.user.hidead = false;nx.webpager = nx.webpager || {};
+nx.production = true;
+</script>
+
+<script type="text/javascript" src="http://s.xnimg.cn/a71272/nx/core/libs.js"></script>
+<script type="text/javascript">
+define.config({map:{
+"backbone":"http://s.xnimg.cn/a71190/nx/core/backbone.js",
+"ui/draggable":"http://s.xnimg.cn/a70750/nx/core/ui/draggable.js",
+"ui/menu":"http://s.xnimg.cn/a70736/nx/core/ui/menu.js",
+"ui/resizable":"http://s.xnimg.cn/a70738/nx/core/ui/resizable.js",
+"ui/sortable":"http://s.xnimg.cn/a70749/nx/core/ui/sortable.js",
+"ui/ceiling":"http://s.xnimg.cn/a70307/nx/core/ui/ceiling.js",
+"ui/columns":"http://s.xnimg.cn/a68070/nx/core/ui/columns.js",
+"ui/dialog":"http://s.xnimg.cn/a70750/nx/core/ui/dialog.js",
+"ui/fileupload":"http://s.xnimg.cn/a70953/nx/core/ui/fileupload.js",
+"ui/pagination":"http://s.xnimg.cn/a70307/nx/core/ui/pagination.js",
+"ui/placeholder":"http://s.xnimg.cn/a68540/nx/core/ui/placeholder.js",
+"ui/progressbar":"http://s.xnimg.cn/a62964/nx/core/ui/progressbar.js",
+"ui/rows":"http://s.xnimg.cn/a62964/nx/core/ui/rows.js",
+"ui/scroll":"http://s.xnimg.cn/a61518/nx/core/ui/scroll.js",
+"ui/scrollbar":"http://s.xnimg.cn/a70150/nx/core/ui/scrollbar.js",
+"ui/select":"http://s.xnimg.cn/a71270/nx/core/ui/select.js",
+"ui/slideshow":"http://s.xnimg.cn/a70528/nx/core/ui/slideshow.js",
+"ui/speech":"http://s.xnimg.cn/a70256/nx/core/ui/speech.js",
+"ui/textbox":"http://s.xnimg.cn/a69485/nx/core/ui/textbox.js",
+"ui/tooltip":"http://s.xnimg.cn/a70685/nx/core/ui/tooltip.js",
+"ui/renren/addfriend":"http://s.xnimg.cn/a64963/nx/core/ui/renren/addFriendLayer.js",
+"ui/renren/at":"http://s.xnimg.cn/a70684/nx/core/ui/renren/at.js",
+"ui/renren/commentCenter":"http://s.xnimg.cn/a71189/nx/core/ui/renren/commentCenter.js",
+"ui/renren/emotion":"http://s.xnimg.cn/a70256/nx/core/ui/renren/emotion.js",
+"ui/renren/friendgroup":"http://s.xnimg.cn/a62964/nx/core/ui/renren/friendGroup.js",
+"ui/renren/friendListSelector":"http://s.xnimg.cn/a71270/nx/core/ui/renren/friendListSelector.js",
+"ui/renren/like":"http://s.xnimg.cn/a70307/nx/core/ui/renren/like.js",
+"nx/namecard":"http://s.xnimg.cn/a70952/nx/core/ui/renren/namecard.js",
+"ui/renren/pagelayer":"http://s.xnimg.cn/a62964/nx/core/ui/renren/pageLayer.js",
+"ui/renren/photoupload":"http://s.xnimg.cn/a71190/nx/core/ui/renren/photoupload.js",
+"ui/renren/privacy":"http://s.xnimg.cn/a70736/nx/core/ui/renren/privacy.js",
+"ui/renren/share":"http://s.xnimg.cn/a71188/nx/core/ui/renren/share.js",
+"ui/renren/vocal":"http://s.xnimg.cn/a70258/nx/core/ui/renren/vocal.js",
+"ui/renren/with":"http://s.xnimg.cn/a70613/nx/core/ui/renren/with.js",
+"ui/clipboard":"http://s.xnimg.cn/a63417/nx/core/ui/clipboard.js",
+"app/publisher":"http://s.xnimg.cn/a71270/nx/core/app/publisher.js",
+"viewer":"http://s.xnimg.cn/a70710/nx/photo/viewer/js/viewer.js",
+"media/player": "http://s.xnimg.cn/nx/photo/viewer/js/mediaplayer.js",
+"ui/renren/like/commentseed":"http://s.xnimg.cn/a64636/nx/core/ui/renren/like.seed.comment.js",
+"ui/renren/like/seed":"http://s.xnimg.cn/a62964/nx/core/ui/renren/like.seed.js",
+"ui/renren/share/seed":"http://s.xnimg.cn/a62964/nx/core/ui/renren/share.seed.js",
+"ui/renren/follow":"http://s.xnimg.cn/a69037/nx/core/ui/renren/follow.js",
+"ui/autocomplete":"http://s.xnimg.cn/a70736/nx/core/ui/autocomplete.js",
+"ui/showCommonFriend":"http://s.xnimg.cn/a63984/nx/core/ui/renren/showcommonfriend.js",
+"photo/circler":"http://s.xnimg.cn/a70631/nx/photo/phototerminal/js/circler.js",
+"ui/friendSearch":"http://s.xnimg.cn/a64338/nx/core/ui/renren/friendSearch.js",
+"ui/renren/replyOption":"http://s.xnimg.cn/a68256/nx/core/ui/renren/replyOption.js",
+"photo/avatarUpload": "http://s.xnimg.cn/a70628/nx/photo/upload-avata/js/avatarUpload.js"
+}});
+nx.data.isDoubleFeed = Boolean(false);
+nx.data.isDoubleFeedGuide = Boolean(false);
+</script>
+
+<script type="text/javascript" src="http://s.xnimg.cn/a72038/nx/core/base.js">
+</script>
+
+<script type="text/javascript">
+nx.user.pageid = 'home';
+//shetuanren guide
+var show_activity_flag40000003 = '' === 'true';
+</script>
+
+<link rel="stylesheet" type="text/css" href="http://s.xnimg.cn/a71891/nx/home/css/home.css" />
+
+</head>
+
+<body>
+
+
+<div id="nxContainer" class="nx-container"><div class="nx-main">
+
+<div id="nxHeader" class="hd-wraper "><div class="hd-fixed-wraper clearfix" style="height: 70px;"><div class="hd-main">
+
+<h1 style="height: 70px;
+width: 200px;
+position: absolute;
+left: 20px;
+top: 0;
+zoom: 1;">
+<div style="background-image: url('img/logo.png');height: 70px; width: 200px;">
+	<a href="homepage.php" title="HappyTrip"></a></div>
+</h1>
+
+<div class="hd-nav clearfix"><dl class="hd-account clearfix">
+	
+	<dt><a href="http://www.renren.com/518341325/profile">
+			<img class="hd-avatar" width="30" height="30" src="img/root.jpg" alt="root" />
+		</a>
+	</dt>
+	
+	<dd>
+		<a class="hd-name" href="f6_setting.php" title="<?php  @session_start();echo $_SESSION["username"];?>"><?php  @session_start();echo $_SESSION["username"];?></a>
+	</dd>
+
+</dl></div>
+
+</div></div></div>
+
+<div class="nx-wraper clearfix">
+
+
+<div class="nx-sidebar"><div id="nxSlidebar" class="slide-fixed-wraper clearfix"><div class="app-nav-wrap"><div class="app-nav-cont"><ul class="app-nav-list">
+
+<li class="app-nav-item app-nav-item-cur app-feed" data-tip="首页">
+	<a class="app-link" href="homepage.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif"  />
+		<span class="app-title">首页</span>
+	</a>
+</li>
+<li class="app-nav-item app-homepage" data-tip="我的旅游">
+	<a class="app-link" href="f1_mytrip.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">我的旅游</span>
+	</a>
+</li>
+
+<li class="app-nav-item app-matter" data-tip="旅游招友">
+	<a class="app-link" href="f2_release_trip.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">旅游招友</span>
+	</a>
+</li>
+
+<li class="app-nav-item app-homepage" data-tip="分享趣事">
+	<a class="app-link" href="f3_release_sence.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">分享趣事</span>
+	</a>
+</li>
+
+
+<li class="app-nav-item app-friends" data-tip="撰写攻略">
+	<a class="app-link" href="f4_release_strategy.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">撰写攻略</span>
+	</a>
+</li>
+
+<li class="app-nav-item app-homepage" data-tip="好友">
+	<a class="app-link" href="f5_friend.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">好友</span>
+	</a>
+</li>
+
+<li class="app-nav-item app-homepage" data-tip="设置">
+	<a class="app-link" href="f6_setting.php">
+		<img class="app-icon" width="32" height="32" src="http://a.xnimg.cn/a.gif" />
+		<span class="app-title">设置</span>
+	</a>
+</li>
+
+</ul></div></div></div></div>
+
+
+<div class="bd-container ">
+<script type="text/javascript">
+(function() {
+define(function(require, exports) {
+var responseFrame = require('responseFrame');
+//responseFrame.initFrameSize();							
+});
+})();
+</script>
+
+
+<div class="bd-main"><div class="bd-content clearfix"><div class="feed-list">
+<div class = "a-feed">
+<h1 style="color:green">发布消息</h1></br>
+<form action="action.php?action=addnews" method="post">
+
+<table align="center" border="0" width="759"> 
+<tr>
+	<TD align="right" width=30 height=15>主题</TD>
+	<td><input type="text" name="topic" height=15 width=120 style="border:1px #FFFFFF solid"></td>
+</tr> 
+<tr>
+	<TD align="right" width=30 height=60>内容</TD>
+	<td><textarea name="context" style=" height:100%;" rows="6" cols="80"></textarea></td>
+</tr> 
+<tr>
+	<td align="right" height=15 colspan="2"><input type="submit" name="submit" value="提交" height=15 width=120></input></td>
+</tr> 
+</table>
+</form>
+</div>
+
+
+</div></div></div>
+
+
+</div>
+
+</div>
+
+</div></div>
+
+
+
+
+
+<div id="toolBackTo" class="back-to" widthlog="">
+<a href="#nogo" onclick="return false;"></a> 
+</div>
+
+<script type="text/javascript"src="http://s.xnimg.cn/a72146/nx/home/js/home-all.js"></script>
+</body>
+
+</html>
