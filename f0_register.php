c563dd25ebac1a8fec6e71fb0921fa70a9b73d69
fencingworld <fencingworld@qq.com>
2014-11-12 12:42:29 +0800

    Create f0_register.php

---
+++ f0_register.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html;charset=GKB" http-equiv="content-type">
+    <title>快乐旅行 - 我们用智能重新定义旅行!</title>
+    <meta name="keywords" content="快乐,快乐旅行,自由行,行程助手">
+    <meta name="description" content="快乐旅行，一键规划专属于你的自由行行程方案。交通，住宿，景点，美食一网打尽，不用再手动订机票，订酒店，查路线，比价，快乐旅行一键帮您搞定。">
+	<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+	
+    <link type="text/css" rel="stylesheet" href="resource/css/mioji.css" />
+    <link type="text/css" rel="stylesheet" href="resource/css/mioji_index.css" />
+
+    <script>  MJ = { uid : 0, phpuid: '544e03d40523c'} </script>
+</head>
+<body class="mioji_index">
+
+	
+<div class="mji_bgAni">
+    <img class="city1" src="resource/images/index/city_1.png">
+    <img class="flight anim" src="resource/images/index/flight.png">
+    <img class="city2" src="resource/images/index/city_21.png">
+    <img class="motian anim" src="resource/images/index/motian.png">
+    <img class="cloud1 anim" src="resource/images/index/cloud1.png">
+    <img class="cloud2 anim" src="resource/images/index/cloud2.png">
+    <img class="cloud3 anim" src="resource/images/index/cloud3.png">
+    <img class="cloud4 anim" src="resource/images/index/cloud4.png">
+</div>
+
+<div class="fInfoBox mji_moveBox">
+
+
+    <div class="mji_fInfo">
+        <div class="logo mji_png"></div>
+		<div class="logo mji_png"></div>
+
+		<h1 style="color:red ">注册</h1>
+
+            <form id="RegisterForm" action="action.php?action=reg" method="post">
+                <div class="row text ">
+                    <label for="RegisterForm_username" class="required" style="display: block;">用户名</label>
+                    <input type="text" class="iText inputFS inputTip bsz" name="username" data-tip="4-20个字符，支持中英文、数字及“_”和“—”的组合" data-veriy="userCheck" data-ajaxveriy="/userajax/checkName">
+                    <span class="info"><i class="mji_png"></i> <i class="msg"></i></span>
+                </div>
+
+                <div class="pw row text">
+                    <label for="RegisterForm_username" class="required" style="display: block;">密码</label>
+                    <input type="password" class="pw iText inputFS inputTip bsz" name="password" data-tip="6-20位的字符，可使用字母、数字和符号的组合" data-veriy="pwCheck" >
+                    <span class="info"><i class="mji_png"></i> <i class="msg"></i></span>
+                </div>
+
+                <div class="pw row text">
+                    <label for="RegisterForm_username" class="required" style="display: block;">邮箱</label>
+                    <input type="text" class="pw iText inputFS inputTip bsz" name="email" data-tip="输入电子邮箱" data-veriy="emailCheck" data-ajaxveriy="/userajax/checkMail">
+                    <span class="info"><i class="mji_png"></i> <i class="msg"></i></span>
+                </div>
+
+                <div class="isAgreeRow row">
+                    <label for="RegisterForm_username" class="isAgree required fl checkBox" style="display: block;">
+                        <span class="selected select epng" data-val="1"></span>
+                    </label>
+                    <span class="fl" style="font-size: 13px">我已阅读并同意 《<a href="/user/agreement/" target="_blank">快乐旅行用户使用协议</a>》</span>
+                    <input type="hidden" class="" name="RegisterForm[isAgree]" value="1" id="RegisterForm_isAgree">
+                    <span class="info"><i class="mji_png"></i> <i class="msg"></i></span>
+                </div>
+
+                <div class="sign row clx">
+                 
+					<input type="submit"  class="mji_commBtn1 commBtn1 bsz green fr" id="signBtn" data-log="10006005"></input>
+                    <span class="info"><i class="mji_png"></i> <i class="msg">此项不能为空</i></span>
+                </div>
+                <a href="javascript:;" data-show="regSucessBox" style="display: none"></a>
+            </form>
+
+    </div>
+
+</div>
+
+
+
+<div class="bottomInfo">
+    @ HappyTrip 2014,All rights reserved
+</div>
+
+
+</body>
+</html>
+<div style="display: none">
+    <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1000327792'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s5.cnzz.com/z_stat.php%3Fid%3D1000327792' type='text/javascript'%3E%3C/script%3E"));</script>
+</div>
