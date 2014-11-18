4fd4897a55d97e7d556cf716e9b7ed4c1ee87992
AuroraFan <aurorafan@163.com>
2014-11-18 22:43:10 +0800

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,81 @@
+
+<ul class="nav nav-list">
+
+<li class="active" >
+	<a   href="homepage.php">
+		<img class="app-icon" width="32" height="32" src="img/menu1.jpg"  />
+		<span class="app-title">首页</span>
+	</a>
+</li>
+<li class="active">
+	<a   href="f1_mytrip.php">
+		<img class="app-icon" width="32" height="32" src="img/menu2.jpg" />
+		<span class="app-title">我的旅游</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f7_hotplaces.php">
+		<img class="app-icon" width="32" height="32" src="img/menu3.jpg" />
+		<span class="app-title">旅游景区</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f2_release_trip.php">
+		<img class="app-icon" width="32" height="32" src="img/menu4.jpg" />
+		<span class="app-title">旅游招友</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f8_trip_group.php">
+		<img class="app-icon" width="32" height="32" src="img/menu5.jpg" />
+		<span class="app-title">旅游组团</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f3_release_sence.php">
+		<img class="app-icon" width="32" height="32" src="img/menu6.jpg" />
+		<span class="app-title">旅游计划</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f3_release_sence.php">
+		<img class="app-icon" width="32" height="32" src="img/menu7.jpg" />
+		<span class="app-title">旅游进展</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f3_release_sence.php">
+		<img class="app-icon" width="32" height="32" src="img/menu8.jpg" />
+		<span class="app-title">旅游分享</span>
+	</a>
+</li>
+
+
+<li class="active">
+	<a   href="f4_release_strategy.php">
+		<img class="app-icon" width="32" height="32" src="img/menu9.jpg" />
+		<span class="app-title">旅游攻略</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="f5_friend.php">
+		<img class="app-icon" width="32" height="32" src="img/menu10.jpg" />
+		<span class="app-title">旅游交友</span>
+	</a>
+</li>
+
+<li class="active">
+	<a   href="<?php @session_start(); echo 'f6_setting.php?id='.$_SESSION['id'] ?>"> 
+		<img class="app-icon" width="32" height="32" src="img/menu11.jpg" />
+		<span class="app-title">设置</span>
+	</a>
+</li>
+
+</ul>
